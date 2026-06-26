52a4d38349a7cb770a35e205ffd712d9f8083ef3
khantphyoemin537-star <khantphyoemin537@gmail.com>
2026-06-26 19:52:16 +0630

    Update shared_vars.py

--- shared_vars.py
+++ shared_vars.py
@@ -27,7 +27,6 @@ from game_manager import GameManager
 from database import db
 
 db.bind('sqlite', os.getenv('UNO_DB', 'uno.sqlite3'), create_db=True)
-db.generate_mapping(create_tables=True)
 
 gm = GameManager()
 updater = Updater(token=TOKEN, workers=WORKERS, use_context=True)
